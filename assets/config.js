5012575d8553e0947822c1330ca45bb8ff4946c4
Cyrnax <65818042+Cyrnax24@users.noreply.github.com>
2021-07-24 16:00:03 +0200

    💎 Creation

---
+++ assets/config.js
@@ -0,0 +1,26 @@
+const config = {
+    USERNAME: "Cyrnaxx.#9999",
+    PICTURE: "assets/img/profile_picture.gif",
+
+    DESCRIPTION: "JavaScript developer. Staff of many servers. Many projects like development. Moderates and administers servers.",
+
+    BADGES: [
+        { id: "HypeSquad Balance", image: "https://discord.com/assets/9fdc63ef8a3cc1617c7586286c34e4f1.svg" },
+        { id: "Nitro Sub", image: "https://discord.com/assets/386884eecd36164487505ddfbac35a9d.svg" },
+        {id: "Nitro Boosting", image: "https://discord.com/assets/fbb6f1e160280f0e9aeb5d7c452eefe1.svg"}
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com/invite/W4z3843KFv", name: "Protect - Senior Analsyte" },
+        { id: "project-2", image: "assets/img/placeholder2.gif", link: "https://discord.com/invite/AaucX7B6xB", name: "Development Community - Administrator" },
+        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com/invite/T777nzH3Ra", name: "Ma Belle France - Helper" },
+        { id: "project-4", image: "assets/img/placeholder4.png", link: "https://discord.com/invite/h622QdhW9H", name: "Art Platform - Responsable" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Cyrnax24" },
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/channel/UCCt1VLzP3rUOoM0dEayzuKA" },
+        { class_name: "fab fa-discord", link: "https://discord.com/invite/MX4R5ymCwY"},
+        { class_name: "fab fa-opera", link: "https://i0.wp.com/emoji.gg/assets/emoji/3859_Loading.gif"}
+    ],
+}

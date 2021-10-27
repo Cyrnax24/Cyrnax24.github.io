065a26539400d77350ee53bb92e9e2447aaed8b8
Cyrnax <65818042+Cyrnax24@users.noreply.github.com>
2021-10-27 22:43:18 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,26 @@
+const config = {
+    USERNAME: "Cyrnaxx.#9999",
+    PICTURE: "assets/img/profile_pictureup.png",
+
+    DESCRIPTION: "~The life is a challenge to be met, a happiness to be earned, an adventure to be attempted.~<br>~La vie est un défi à relever, un bonheur à mériter, une aventure à tenter.~",
+
+    BADGES: [
+        { id: "HypeSquad Balance", image: "https://cdn.discordapp.com/emojis/902664136897474570.png" },
+        { id: "Nitro Sub", image: "https://cdn.discordapp.com/emojis/903018530176397352.png" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com/invite/W4z3843KFv", name: "Protect - Senior Analsyte" },
+        { id: "project-2", image: "assets/img/placeholder2.gif", link: "https://discord.com/invite/AaucX7B6xB", name: "Development Community - Administrator" },
+        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com/invite/T777nzH3Ra", name: "Ma Belle France - Helper" },
+        { id: "project-4", image: "assets/img/placeholder4.png", link: "https://discord.com/invite/h622QdhW9H", name: "Art Platform - Responsable" },
+        { id: "project-5", image: "assets/img/placeholder5.png", link: "https://discord.com/moderation", name: "Discord Moderator Academy" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Cyrnax24" },
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/channel/UCCt1VLzP3rUOoM0dEayzuKA" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/Cyrnax24" },
+        { class_name: "fab fa-opera", link: "https://i0.wp.com/emoji.gg/assets/emoji/3859_Loading.gif"}
+    ],
+}

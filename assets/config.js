fed044c2ecd82e8d6f9877bf3de3ae9902ca4440
Cyrnaxx <65818042+Cyrnax24@users.noreply.github.com>
2022-10-02 18:08:46 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,31 @@
+const config = {
+    USERNAME: "Cyrnaxx.#9999",
+    PICTURE: "assets/img/profile_pictureup.png",
+
+    DESCRIPTION: "I'm a JavaScript developer, I moderate and administer a lot of communities and large servers. Je parle nativement Français, I speak English, hablo español.",
+
+    BADGES: [
+        { id: "Discord Certified Moderator", image: "https://cdn.discordapp.com/emojis/921744998054117426.png" },
+        { id: "HypeSquad Balance", image: "https://cdn.discordapp.com/emojis/902664136897474570.png" },
+        { id: "Nitro Sub", image: "https://cdn.discordapp.com/emojis/903018530176397352.png" },
+        { id: "Nitro Boost", image: "https://cdn.discordapp.com/emojis/979198267197906984.gif" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com/invite/W4z3843KFv", name: "Protect - Senior Analsyte" },
+        { id: "project-2", image: "assets/img/placeholder2.gif", link: "https://discord.com/invite/AaucX7B6xB", name: "Development Community - Manager" },
+        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.gg/yFDzwdAq", name: "Larzize - Moderator" },
+        { id: "project-4", image: "assets/img/placeholder4.png", link: "https://discord.com/invite/w5NCQPy6sT", name: "P2N Bot - Owner" },
+        { id: "project-5", image: "assets/img/placeholder5.png", link: "https://discord.com/moderation", name: "Discord Moderator Academy" },
+        { id: "project-6", image: "assets/img/placeholder6.png", link: "https://discord.gg/wyAGWwq7nW", name: "NIGHT-API - Owner" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/Cyrnax24" },
+        { class_name: "fab fa-youtube", link: "https://www.youtube.com/channel/UCCt1VLzP3rUOoM0dEayzuKA" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/Cyrnax24" },
+        { class_name: "fab fa-opera", link: "https://Cyrnax24.github.io/" },
+        { class_name: "fab fa-instagram", link: "https://instagram.com/cyrnax_discord24" },
+        { class_name: "fab fa-spotify", link: "https://open.spotify.com/user/phd6s3yf8u7p2xiosz1qrppqc" }
+    ],
+}
